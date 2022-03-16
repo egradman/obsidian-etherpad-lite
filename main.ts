4c927e70cccf8ff038fdd81dd9f2942daff6f0eb
Eric Gradman <egradman@Erics-MacBook-Pro.local>
2022-03-16 15:16:33 -0700

    unregister file-open.  desktopOnly

--- main.ts
+++ main.ts
@@ -52,9 +52,11 @@ export default class Etherpad extends Plugin {
     const statusBarItemEl = this.addStatusBarItem();
     statusBarItemEl.setText('Status Bar Text');
 
-    this.app.workspace.on('file-open', async (note)=>{
-      this.replace_note_from_etherpad(note);
-    });
+    this.registerEvent(
+      this.app.workspace.on('file-open', async (note)=>{
+        this.replace_note_from_etherpad(note);
+      })
+    );
 
     // This adds an editor command that can perform some operation on the current editor instance
     this.addCommand({
@@ -109,37 +111,9 @@ export default class Etherpad extends Plugin {
       }
     });
 
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
-    //this.addCommand({
-    //  id: 'open-sample-modal-complex',
-    //  name: 'Open sample modal (complex)',
-    //  checkCallback: (checking: boolean) => {
-    //    // Conditions to check
-    //    const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-    //    if (markdownView) {
-    //      // If checking is true, we're simply "checking" if the command can be run.
-    //      // If checking is false, then we want to actually perform the operation.
-    //      if (!checking) {
-    //        new EtherpadModal(this.app).open();
-    //      }
-
-    //      // This command will only show up in Command Palette when the check function returns true
-    //      return true;
-    //    }
-    //  }
-    //});
-
     // This adds a settings tab so the user can configure various aspects of the plugin
     this.addSettingTab(new EtherpadSettingTab(this.app, this));
 
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    //this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-    //  console.log('click', evt);
-    //});
-
-    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    //this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
   }
 
   onunload() {
@@ -208,7 +182,6 @@ export default class Etherpad extends Plugin {
         let note_html = data.html;
 
         let note_text = td.turndown(note_html)
-        console.log(data.html);
         this.app.vault.modify(note, frontmatter_text + note_text);
         let url = this.get_url_for_pad_id(frontmatter.etherpad_id);
         new Notice(`Note was reloaded from ${url}.\nLocal edits will be discarded!`);
